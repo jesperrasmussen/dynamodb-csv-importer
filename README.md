1b0d7013e4b501660292a01e1e0706f8f9fee1c2
Jesper Rasmussen <me@jesperrasmussen.com>
2019-11-11 12:28:35 +0100

    Uses region from ENV rather than hardcoding

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # play-ovp-dynamodb-csv-importer
 Simple set of scripts to import data into DynamoDB
 
+Currently requires the environment variable `AWS_SDK_LOAD_CONFIG` to be set to true, in order to load region etc. from configs.
+
 Call using:
 
 ```
--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var args = process.argv.slice(2);
 
 var credentials = new AWS.SharedIniFileCredentials({ profile: `${args[0]}` });
 AWS.config.credentials = credentials;
-
-AWS.config.update({ region: 'eu-central-1' });
 const docClient = new AWS.DynamoDB.DocumentClient()
 const contents = fs.readFileSync(`${args[2]}`, 'utf-8')
 // If you made an export of a DynamoDB table you need to remove (S) etc from header
