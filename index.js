c1167e652acc8304eef265377ce4df343891b1d8
Jesper Rasmussen <me@jesperrasmussen.com>
2019-11-12 12:46:13 +0100

    Switches to using Papaparse for handling CSV parsing, skipping subobjects for now

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const fs = require('fs')
-const parse = require('csv-parse/lib/sync')
 const AWS = require('aws-sdk')
+const Papa = require('papaparse')
 
 var args = process.argv.slice(2);
-
 var credentials = new AWS.SharedIniFileCredentials({ profile: `${args[0]}` });
+
 AWS.config.credentials = credentials;
+
 const docClient = new AWS.DynamoDB.DocumentClient()
 const contents = fs.readFileSync(`${args[2]}`, 'utf-8')
 // If you made an export of a DynamoDB table you need to remove (S) etc from header
-const data = parse(contents, { columns: true })
-
+const data = Papa.parse(contents, { header: true, dynamicTyping: true }).data;
 
 data.forEach((item) => {
     if (!item.maybeempty) delete item.maybeempty //need to remove empty items
     for (var prop in item) {
         if (typeof item[prop] === 'object') {// dive deeper in
-            removeEmptyStringElements(obj[prop]);
+            delete item[prop];
+            //removeEmptyStringElements(obj[prop]);
         } else if (item[prop] === '') {// delete elements that are empty strings
             delete item[prop];
         }
     }
+
     docClient.put({ TableName: `${args[1]}`, Item: item }, (err, res) => {
         if (err) console.log(err)
     })
